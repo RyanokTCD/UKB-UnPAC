7600941a3d14668ad145853d6d039038e1ad5122
RyanokTCD <168449079+RyanokTCD@users.noreply.github.com>
2024-11-05 14:51:54 +0000

    Update UnZipper.py

--- 1. UnZipper Function/UnZipper.py
+++ 1. UnZipper Function/UnZipper.py
@@ -8,9 +8,8 @@ import time
 
 #UnZipper---------------------------------------------------------------------------------------------------------------------------------
 def UnZipper (Path):
-    rootPath = Path
     pattern = '*.zip'
-    for root, dirs, files in os.walk(rootPath):
+    for root, dirs, files in os.walk(Path):
         for filename in fnmatch.filter(files, pattern):
             print(os.path.join(root, filename))
             zipfile.ZipFile(os.path.join(root, filename)).extractall(os.path.join(root, os.path.splitext(filename)[0]))
